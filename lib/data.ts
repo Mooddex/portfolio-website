d96d90269ae692a8fb3ac85afa0871b74873667f
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-10-01 15:50:02 +0300

    Refactor image imports to use local paths for consistency

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,8 @@
 import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
-import corpcommentImg from "@/public/2025-07-16-000244-create-next-app-screenclip.png";
-import rmtdevImg from "@/public/screencapture-brempatch-dashboard-vercel-app-2025-08-19-18_06_52.png";
-import wordanalyticsImg from "@/public/2025-07-08-181343-my-office-screenclip.png";
+const corpcommentImg = "/2025-07-16-000244-create-next-app-screenclip.png";
+const rmtdevImg = "/screencapture-brempatch-dashboard-vercel-app-2025-08-19-18_06_52.png";
+const wordanalyticsImg = "/2025-07-08-181343-my-office-screenclip.png";
 
 export const links = [
   {
