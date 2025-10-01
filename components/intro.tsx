6328d83c2afa78ee32112e61c562f7e4012734d5
Mahmoud Salama <100090525+Mooddex@users.noreply.github.com>
2025-10-01 17:55:31 +0300

    Update introduction text for clarity and impact

--- components/intro.tsx
+++ components/intro.tsx
@@ -65,7 +65,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Mahmoud.</span> I'm a{" "}
         <span className="font-bold">Front-End developer</span> with{" "}
-        <span className="font-bold">Many years</span> of experience. I enjoy
+        <span className="font-bold">Big Ambitions</span> and experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
