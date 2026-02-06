111f057b3aff25bf500760f9e4ac5a109965fe7e
Mahmoud <msalama027@gmail.com>
2026-02-06 15:21:08 +0200

    improveing

---
+++ components/aboutMotionWrapper.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function AboutMotionWrapper() {
+    return(
+         <motion.div
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.2 }}
+    >
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Law</span>, I decided to pursue my
+        passion for programming. I learned{" "}
+        <span className="font-medium">front-end web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        is{" "}
+        <span className="font-medium">
+          React, Next.js, Vue, Nuxt.js and TypeScript .
+          <span className="underline">My Practices</span> include Responsive Design, REST APIs, Agile (SCRUM), OOP.
+        </span>
+        . I am also familiar with Node.js, Express.js, MongoDB, PostgreSQL and Postman . I am always looking to
+        learn new technologies. I am currently looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games, watching movies, and playing Ping pong. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>. I'm also
+        learning Swimming.
+      </p>
+    </motion.div>
+    )
+}
